18e8be59ba8d54b0f1677efc7fda7662a7b13703
logonrm <logonrm@L2704MICRO28.fiap.com.br>
2017-06-05 21:38:56 -0300

    Teste

--- src/br/com/roteiro6/entity/Funcionario.java
+++ src/br/com/roteiro6/entity/Funcionario.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Cacheable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,11 +19,16 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
-@Table(name = "FUNCIONARIO", catalog = "DBTarefa", uniqueConstraints = {
+@Table(name = "FUNCIONARIO", catalog = "DBTarefas", uniqueConstraints = {
 		@UniqueConstraint(columnNames = "CODIGO_FUNCIONARIO") })
 @NamedQuery(name = "Funcionario.findAll", query = "select f from Funcionario f")
 
+@Cacheable
+@Cache(region="func", usage=CacheConcurrencyStrategy.READ_ONLY)
 public class Funcionario implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -39,7 +45,7 @@ public class Funcionario implements Serializable {
 	private String nome;
 	
 	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinTable(name = "FUNCIONARIO_TAREFA", catalog = "DBTarefa", joinColumns = {
+	@JoinTable(name = "FUNCIONARIO_TAREFA", catalog = "DBTarefas", joinColumns = {
 			@JoinColumn(name = "FUNCIONARIO_ID", nullable = false, updatable = false) }, inverseJoinColumns = {
 					@JoinColumn(name = "TAREFA_ID", nullable = false, updatable = false) })
 	private Set<Tarefa> tarefas = new HashSet<>();
